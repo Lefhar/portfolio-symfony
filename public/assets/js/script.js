066f1518e4d02216bc2bbed6d8fe8653722f1e88
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-10-11 07:36:54 +0200

    mise à jour zone admin

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,21 @@
+const sidebarToggle = document.body.querySelector('#sidebarToggle');
+if (sidebarToggle) {
+    // Uncomment Below to persist sidebar toggle between refreshes
+    // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+    //     document.body.classList.toggle('sb-sidenav-toggled');
+    // }
+    sidebarToggle.addEventListener('click', event => {
+        event.preventDefault();
+        document.body.classList.toggle('sb-sidenav-toggled');
+        localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+    });
+}
+
 if (document.getElementsByClassName('intro')) {
     window.addEventListener("DOMContentLoaded", (event) => {
         animate_text();
+
+
     });
 
 // -------------------
